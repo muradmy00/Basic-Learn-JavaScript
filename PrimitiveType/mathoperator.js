3698257d911d0f6f24260258e1b9f608446dc53d
muradmy00 <murad.my00@gmail.com>
2024-05-19 10:38:42 +0600

    all

--- PrimitiveType/mathoperator.js
+++ PrimitiveType/mathoperator.js
@@ -18,12 +18,14 @@ console.log(result);
 
 
 
-p=2.88;
+p=2.58;
 
 console.log(Math.round(p));
 console.log(Math.ceil(p));
 console.log(Math.floor(p));
 
 
+
 x=-55;
 console.log(Math.abs(x));
+// return the absolute value
